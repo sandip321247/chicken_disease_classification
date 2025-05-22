e1bc3937769f52322a5ad60eca2f276cf460e254
sandip_behera <sandipbehera321@gmail.com>
2025-05-22 23:22:48 +0530

    Data Ingestion Completed

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,6 @@
+# variable or file which we are using one time and won't be changing
+
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
